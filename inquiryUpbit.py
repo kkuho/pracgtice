433b1209bff0b6dc810f69bc736773590b3dabfd
kkuho <jungkuho@gmail.com>
2022-02-09 11:17:44 +0900

    자동 매매 ver.2

--- inquiryUpbit.py
+++ inquiryUpbit.py
@@ -23,12 +23,11 @@ def get_target_price(ticker): # 변동성 돌파 구간 계산
     return target
 
 def buy_crypto_currency(ticker): # 매수
-    krw = upbit.get_balance() - 1000 # 예수금 확인
-    orderbook = pyupbit.get_orderbook(ticker)
-
-    asks = orderbook['orderbook_units']
-    sell_price = asks[0]['ask_price']
-    unit = krw / float(sell_price) # float 은 실수형으로 만들어주는 함수
+    krw = upbit.get_balance()*0.999 # 예수금 확인
+    # orderbook = pyupbit.get_orderbook(ticker)
+    # asks = orderbook['orderbook_units']
+    # buy_price = asks[0]['ask_price']
+    # unit = krw / float(buy_price) # float 은 실수형으로 만들어주는 함수
     buy = upbit.buy_market_order(ticker, krw) # 현재 시장가로 구매 가능한 수량을 구매 ! 주의!! 실행하는 순간 매수됨
     return buy
 
@@ -37,8 +36,6 @@ def sell_crypto_currency(ticker): # 매도
     sell = upbit.sell_market_order(ticker, unit_sell)
     return sell
     
-
-
 def get_yesterday_ma5(ticker): # 5일 평균선 
     df = pyupbit.get_ohlcv(ticker)
     close = df['close']
@@ -52,7 +49,8 @@ def write_trade(trade): # trade 정보를 엑셀로 기록하는 함수
     row = []
     day = trade['created_at'].split('T')[0] # 날짜
     time_action = trade['created_at'].split('T')[1].split('+')[0]
-    
+    bal = upbit.get_balances
+
     row.append(day)
     row.append(time_action)
 
@@ -62,11 +60,17 @@ def write_trade(trade): # trade 정보를 엑셀로 기록하는 함수
     if trade['side'] == 'ask': 
         row.append('매도')
         row.append(trade['volume'])
-        row.append(trade['uuid']) 
+        row.append(trade['uuid'])
+        row.append(bal[1]['avg_buy_price'])
+        row.append(bal[1]['balance']) 
+
     else : 
         row.append('매수') 
         row.append(trade['price'])
         row.append(trade['uuid'])
+        row.append(bal[1]['avg_buy_price'])
+        row.append(bal[1]['balance'])
+        
 
     ws.append(row)
     wb.save('upbitRecord.xlsx')
@@ -99,23 +103,34 @@ while True:
         now = datetime.datetime.now()
         current_price = pyupbit.get_current_price(ticker_input)
         krw = upbit.get_balance()
+        myval = upbit.get_balances()
+        bought_coin = False
         
         if mid < now < mid + datetime.timedelta(seconds=10): # 9시에서 10초 내에 있을 때 9시로 간주함
-            print("정각입니다!!")
+            print("Update!!")
             target_price = get_target_price(ticker_input)
             ma5 = get_yesterday_ma5(ticker_input)
             now = datetime.datetime.now()
             mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(hours=33)
-            trade = sell_crypto_currency(ticker_input)
-            write_trade(trade)
-            write_target(ticker_input, target_price, ma5, now)
-        
-        elif (current_price > target_price) and (current_price > ma5) and (krw >1000):
+
+
+        if (current_price > target_price) and (current_price > ma5) and (krw >1000):
             
             print("가즈아아아!~~~")
             trade = buy_crypto_currency(ticker_input)
+            bought_coin = True
+
             write_trade(trade)
             write_target(ticker_input, target_price, ma5, now)
+
+
+        # 현재가가 매수 평균가보다 3% 이상일 때 매도
+        
+        if bought_coin == True and (current_price > float(myval[1]['avg_buy_price']) * 1.03) :
+            trade = sell_crypto_currency(ticker_input)
+            bought_coin = False
+            write_trade(trade)
+        
 
         else:
             print(now, "|", "현재가 : " , current_price, "|", "목표가 : ", target_price, "|",  "5일선 평균가 : ", ma5)
--- test1.py
+++ test1.py
@@ -1,16 +1,33 @@
 import pyupbit
-import datetime as dt
+import time
+import datetime
 
-df = pyupbit.get_ohlcv('KRW-ETH', interval="minute10", count=10).drop_duplicates()
-dff = df.reset_index().sort_values(by="index", ascending=False)
-dfff = dff.head(1)['index'].item().hour
-a = dff.iloc[0]
-#print(df)
-print(dff)
-print(dfff)
-print(a)
+global coinlist
+
+tickers = pyupbit.get_tickers(fiat="KRW")
+#print(tickers[1:11])
+curtime = datetime.datetime.now()
+coinlist = []
 
 
-curtime = dt.datetime.now().hour
-print(curtime%24)
-#pyupbit.get_ohlcv(item, interval="minute10", count=10).drop_duplicates()
+for item in tickers[1:6] :
+    h_itemlists_prev = pyupbit.get_ohlcv(item, interval="minute10", count=10).drop_duplicates()
+    h_itemlists = h_itemlists_prev.reset_index().sort_values(by="index", ascending=False) # ticker의 ohlcv 값을 index를 reset해서 내림차순으로 정렬
+    #print(h_itemlists)
+
+    if( (h_itemlists.head(1)['index'].item().hour)%24 == curtime.hour) : # 첫번째 기록의 now date기록 중에 시간만 추출해서 지금 시간과 같다면
+
+        coinlist.append([ item, 'null', 0, 0, curtime, h_itemlists.iloc[1]['value'] ]) # 두번째 데이터를 기록하네?? 왜?? 암튼 ticker별로 총거래금액을 기록
+        
+    #print(coinlist)
+
+    time.sleep(0.1)
+
+sortlist = sorted(coinlist, key=lambda x:x[5], reverse= True)
+#print(sortlist)
+buyflag = True
+
+for idx, x in enumerate(sortlist) :
+    if(buyflag and idx < 10) :
+        print(idx, ":", x)
+        
